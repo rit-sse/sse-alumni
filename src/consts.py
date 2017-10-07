c03e5ef2342994d7cee1c293f05a4e82ef9d6293
Wade Mauger <wadeanthony0100@gmail.com>
2017-10-07 19:36:56 -0400

    Initial layout idea

--- src/consts.py
+++ src/consts.py
@@ -1,6 +1,7 @@
 TEMPLATES_PATH = './templates'
 TEMPLATES_FILTER = lambda x: x != 'base.html'
 TEMPLATES_MAP = lambda x: x.split('.')[0]
+STATIC_PATH = './templates/static'
 
 def getPages(loader):
     return list( map(TEMPLATES_MAP, filter(TEMPLATES_FILTER, loader.list_templates())) )
