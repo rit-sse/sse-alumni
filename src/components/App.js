2fdfd20761d308a2a24f4660a8a85e8634872dd2
DanTheManGude <djg4755@rit.edu>
2018-03-27 01:49:56 -0400

    Add routing for the existing pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import Home from './Home';
 import Search from './Search';
 import Login from './Login';
@@ -6,20 +10,25 @@ import Login from './Login';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div class="masthead clearfix">
-              <div class="inner">
-                  <h3 class="masthead-brand">SSE Alumni Directory</h3>
-                  <nav class="nav nav-masthead">
-                      <a class="nav-link" href="/home">Home</a>
-                      <a class="nav-link" href="/search">Search</a>
-                      <a class="nav-link" href="/login">Login</a>
-                  </nav>
-              </div>
-          </div>
+        <div className="App">
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">SSE Alumni Directory</h3>
+                    <nav class="nav nav-masthead">
+                        <Link class="nav-link" to='/home'>Home</Link>
+                        <Link class="nav-link" to='/search'>Search</Link>
+                        <Link class="nav-link" to='/login'>Login</Link>
+                    </nav>
+                </div>
+            </div>
 
-          <Home />
-      </div>
+            <Switch>
+                <Route exact path='/home' component={Home}/>
+                <Route exact path='/search' component={Search}/>
+                <Route exact path='/login' component={Login}/>
+                <Route path='/' render={() => (<Redirect to="/home"/>)}/>
+            </Switch>
+        </div>
     );
   }
 }
