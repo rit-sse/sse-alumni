c179bc4f2f5ec287f87b3a8081114ebd461c52f6
Cameron Braverman <bravermancj@gmail.com>
2018-04-28 12:04:49 -0400

    added heatmap

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,22 @@ import GoogleMapReact from 'google-map-react';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 
+let alumni = [
+  {
+    lat: 37.0902,
+    lng: -122.4194,
+    name: "Derek Erdmann"
+  },
+  {
+    lat: 37.0902,
+    lng: -124.4194,
+    name: "Michael Timbrook"
+  }
+]
+
+
+
+
 
 class Map extends Component {
     static defaultProps = {
@@ -14,55 +30,106 @@ class Map extends Component {
       };
 
 
+
+      onSubmit = (e) => {
+        e.preventDefault();
+        const { address, name } =  this.refs;
+        const key = "AIzaSyBhgwDn93qxpxT9VBR-CTXA4m4qtgN--JY";
+
+        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${key}`)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          return responseJson.movies;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      }
+
+
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          heatmapLibrary={true}
-          heatmap={{
-            positions: [
-              {
-                lat: 37.0902,
-                lng: -97.7129,
-              },
-              {
-                lat: 37.0902,
-                lng: -97.7129,
-              },
-              {
-                lat: 37.7749,
-                lng: -122.4194,
-              },
-              {
-                lat: 37.7749,
-                lng: -122.4194,
-              },
-              {
-                lat: 37.7749,
-                lng: -122.4194,
-              },
-              {
-                lat: 37.7749,
-                lng: -122.4194,
-              },
-              
-            ],
-            options: {
-              radius: 40,
-              opacity: 0.8,
-              gradient: [
-                'rgba(255, 0, 255, 0)',
-                'rgba(255, 0, 255, 1)',
-                
-              ]
-            },
-          }}
-        >
+      <div className="container-fluid" style={{ height: '95vh', paddingTop: '2.5vh' }}>
 
-        </GoogleMapReact>
+            <div className="row">
+            <div className="col-6" style={{ paddingLeft: 0 }}>
+           <div style={{ height: '95vh'}}>
+              <GoogleMapReact
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                heatmapLibrary={true}
+                heatmap={{
+                  positions: [
+                    {
+                      lat: 37.0902,
+                      lng: -97.7129,
+                    },
+                    {
+                      lat: 37.0902,
+                      lng: -97.7129,
+                    },
+                    {
+                      lat: 37.7749,
+                      lng: -122.4194,
+                    },
+                    {
+                      lat: 37.7749,
+                      lng: -122.4194,
+                    },
+                    {
+                      lat: 37.7749,
+                      lng: -122.4194,
+                    },
+                    {
+                      lat: 37.7749,
+                      lng: -122.4194,
+                    },
+                    
+                  ],
+                  options: {
+                    radius: 40,
+                    opacity: 0.8,
+                    gradient: [
+                      'rgba(255, 0, 255, 0)',
+                      'rgba(255, 0, 255, 1)',
+                      
+                    ]
+                  },
+                }}
+              >
+              </GoogleMapReact>
       </div>
+
+            </div>
+            <div className="col-6">
+
+              <form action="POST" onSubmit={this.onSubmit}>
+
+              <div className="form-group" style={{ height: '95vh', paddingTop: '25vh' }}>         
+              <label htmlFor="">Name: </label>
+              <input className="form-control" ref="name" type="text"/>    
+              <br/> 
+
+              <label htmlFor="">Address: </label>
+              <input className="form-control" ref="address" type="text"/> 
+              <br/>
+
+              <button type="submit" className="btn btn-primary"> Submit </button>
+              
+              </div>
+              
+              </form>
+
+
+              </div>
+       
+
+          </div>
+      
+      </div>
+
+
     );
   }
 }
